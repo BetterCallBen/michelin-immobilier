f3709f695911f0890a502f7f43463ad4e54c5ac7
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-02-25 03:55:56 +0100

    debut mailer

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,6 +6,5 @@ class Club < ApplicationRecord
   has_one_attached :bg_image_phone
 
   validates :name, presence: true
-  validates :bg_image, presence: true
-  validates :bg_image_phone, presence: true
+
 end
