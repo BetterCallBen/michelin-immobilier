a8b9a4aebcaada3cc306afaa49a9926aab447239
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-02 05:39:37 +0100

    on essaye de fix le mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://michelin-immobilier-estimation.com" }
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp@gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -109,18 +121,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: "michelin-immobilier-estimation.com", protocol: "https" }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp@gmail.com',
-    port: 587,
-    domain: 'michelin-immobilier-estimation.com',
-    user_name: "benjbdk@gmail.com",
-    password: "jxmvndpcwbgvqufo",
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
 end
