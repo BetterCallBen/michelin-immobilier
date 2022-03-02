2b8662d3047fc37bffd619ad9ebcb40ec58aabe3
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-02 06:41:02 +0100

    heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "michelin-immobilier-estimation.com", protocol: "https" }
+  config.action_mailer.default_url_options = { host: "michelin-immobilier.herokuapp.com", protocol: "https" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
