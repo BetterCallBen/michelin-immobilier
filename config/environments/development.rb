d5fe6aafbbdc76223af0568ec3ed22ad2bfb9acb
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-02 05:50:33 +0100

    same

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
     port: 587,
-    domain: 'http://localhost:3000',
+    domain: 'gmail.com',
     user_name: "benjbdk@gmail.com",
     password: "jxmvndpcwbgvqufo",
     authentication: "plain",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
     port: 587,
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
+    domain: 'gmail.com',
+    user_name: "benjbdk@gmail.com",
+    password: "jxmvndpcwbgvqufo",
     authentication: "plain",
     enable_starttls_auto: true
   }
