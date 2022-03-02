b3e1287fac670f2bf98a1bf11c5905980e41742c
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-02 06:28:46 +0100

    dernier esssai

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "michelin-immobilier.herokuapp.com", protocol: "https" }
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: "https://michelin-immobilier-estimation.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
@@ -13,6 +11,8 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true
   }
+
+  config.action_mailer.raise_delivery_errors = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
