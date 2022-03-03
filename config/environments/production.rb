a0a52b0617052b468a4f9be86384cfee7993be5a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-03 18:05:18 +0100

    jojo fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
     port: 587,
-    domain: "gmail.com",
+    domain: "benjbdk@gmail.com",
     user_name: "benjbdk@gmail.com",
     password: "jxmvndpcwbgvqufo",
     authentication: "plain",
