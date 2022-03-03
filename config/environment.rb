72c5493b2519c62d739e53c2e6736214c72ecc18
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-03 21:59:35 +0100

    secure api

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: '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', # This is the secret sendgrid API key which was issued during API key creation
+  password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
   domain: 'michelin-immobilier-estimation.com',
   address: 'smtp.sendgrid.net',
   port: 587,
