a7b0b2a5dba3578ca963a1a933b6edb319c670e7
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-03 17:58:15 +0100

    try fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
     port: 587,
+    domain: "gmail.com",
     user_name: "benjbdk@gmail.com",
     password: "jxmvndpcwbgvqufo",
     authentication: "plain",
